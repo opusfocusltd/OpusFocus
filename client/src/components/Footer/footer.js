db2089f529004b5c65e4fe9d2863265107ad2ddb
opusfocusltd <opusfocusltd@gmail.com>
2021-11-29 19:36:57 +0530

    footerFix

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -16,35 +16,19 @@ const Footer = () => {
             <FooterLinksWrapper>
               <FooterLinkItems>
                 <FooterLinkTitle>About Us</FooterLinkTitle>
-                <FooterLink to="/signin">How it works</FooterLink>
-                <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Careers</FooterLink>
-                <FooterLink to="/signin">Terms of Service</FooterLink>
+                <FooterLink to="/login">How it works</FooterLink>
+                <FooterLink to="/login">Testimonials</FooterLink>
+                <FooterLink to="/login">Careers</FooterLink>
+                <FooterLink to="/login">Terms of Service</FooterLink>
               </FooterLinkItems>
               <FooterLinkItems>
                 <FooterLinkTitle>Contact Us</FooterLinkTitle>
-                <FooterLink to="/signin">Contact</FooterLink>
-                <FooterLink to="/signin">Support</FooterLink>
-                <FooterLink to="/signin">Address</FooterLink>
-                <FooterLink to="/signin">Sponsorships</FooterLink>
+                <FooterLink to="/login">Contact</FooterLink>
+                <FooterLink to="/login">Support</FooterLink>
+                <FooterLink to="/login">Address</FooterLink>
+                <FooterLink to="/login">Sponsorships</FooterLink>
               </FooterLinkItems>
             </FooterLinksWrapper>
-            {/* <FooterLinksWrapper>
-              <FooterLinkItems>
-                <FooterLinkTitle>About Us</FooterLinkTitle>
-                <FooterLink to="/signin">aassdd</FooterLink>
-                <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Careers</FooterLink>
-                <FooterLink to="/signin">Terms of Service</FooterLink>
-              </FooterLinkItems>
-              <FooterLinkItems>
-                <FooterLinkTitle>Social Media</FooterLinkTitle>
-                <FooterLink to="/signin">Instagram</FooterLink>
-                <FooterLink to="/signin">Facebook</FooterLink>
-                <FooterLink to="/signin">Twitter</FooterLink>
-                <FooterLink to="/signin">Youtube</FooterLink>
-              </FooterLinkItems>
-            </FooterLinksWrapper> */}
           </FooterLinksContainer>
           <SocialMedia>
             <SocialMediaWrap>
@@ -53,16 +37,16 @@ const Footer = () => {
               </SocialMediaLogo>
               <Rights>Opus Focus © {new Date().getFullYear()} All rights reserved.</Rights>
               <SocialIcons>
-                <SocialIconLink href='/home' target='_blank' aria-label='Facebook'>
+                <SocialIconLink href='https://www.facebook.com/' target='_blank' aria-label='Facebook'>
                   <FaFacebook />
                 </SocialIconLink>
-                <SocialIconLink href='/home' target='_blank' aria-label='Instagram'>
+                <SocialIconLink href='https://www.instagram.com/' target='_blank' aria-label='Instagram'>
                   <FaInstagram />
                 </SocialIconLink>
-                <SocialIconLink href='/home' target='_blank' aria-label='Twitter'>
+                <SocialIconLink href='https://twitter.com/?lang=en' target='_blank' aria-label='Twitter'>
                   <FaTwitter />
                 </SocialIconLink>
-                <SocialIconLink href='/home' target='_blank' aria-label='Linkedin'>
+                <SocialIconLink href='https://www.linkedin.com/' target='_blank' aria-label='Linkedin'>
                   <FaLinkedin />
                 </SocialIconLink>
               </SocialIcons>
